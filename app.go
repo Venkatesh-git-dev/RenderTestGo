9c5f1053650bf4e7769021f6f6e252dbc1f2f851
venkatesh9 <k.venkatesh.rules@gmail.com>
2024-12-21 22:45:09 +0530

    added webpage

--- app.go
+++ app.go
@@ -11,8 +11,16 @@ func main() {
 	port:=os.Args[1]
 	fmt.Println(port)
 
+	data,err := os.ReadFile("./index.html")
+	if err!=nil{
+		panic("could not open the file")
+	}
+
+
+
+
 	http.HandleFunc("/",func(w http.ResponseWriter, r *http.Request){
-		fmt.Fprintf(w,"<h1>hello world</h1>")
+		fmt.Fprintf(w,string(data))
 	})
 
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s",port),nil))
