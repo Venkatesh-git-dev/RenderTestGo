3448a92129d4ebb716a82b74db085bf23a280793
venkatesh9 <k.venkatesh.rules@gmail.com>
2024-12-21 22:26:58 +0530

    created go builds

--- app.go
+++ app.go
@@ -4,13 +4,17 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
+	port:=os.Args[1]
+	fmt.Println(port)
+
 	http.HandleFunc("/",func(w http.ResponseWriter, r *http.Request){
 		fmt.Fprintf(w,"<h1>hello world</h1>")
 	})
 
-	log.Fatal(http.ListenAndServe(":8080",nil))
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s",port),nil))
 
 }
